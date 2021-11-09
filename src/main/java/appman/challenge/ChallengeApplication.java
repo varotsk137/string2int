5251c410e53a11d7c3a5949be0f2b1c361a30c1f
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-11-09 16:56:47 +0700

    Challenge Accept: Init Springboot with maven project.

---
+++ src/main/java/appman/challenge/ChallengeApplication.java
@@ -0,0 +1,15 @@
+package appman.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApplication.class, args);
+		
+		System.out.println("Challenge Accept");
+	}
+
+}
